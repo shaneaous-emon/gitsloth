6d8084d2c8b37e06757298ddec5fd91bf55bba82
Francesco Sacco <francescosacco.github@gmail.com>
2026-03-09 17:06:14 +0100

    feat: add version and author info to the codebase

--- gitsloth/__init__.py
+++ gitsloth/__init__.py
@@ -4,5 +4,6 @@ Gitsloth
 AI-powered Git commit message generator enforcing Conventional Commits
 """
 
+# Specifing version and author info for the codebase
 __version__ = "1.0.0"
 __author__ = "Francesco Sacco"
